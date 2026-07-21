791369156293c5d1b1a29a82f22ae1dad6cb3260
Ben Chen <idben.chen@gmail.com>
2026-07-21 15:18:36 +0800

    # 更改欄位名稱

--- csv01/pd_csv01.py
+++ csv01/pd_csv01.py
@@ -4,4 +4,18 @@ pd.set_option("display.unicode.east_asian_width", True)
 # 讀取外部 .csv 檔做為資料來源
 order_df = pd.read_csv("orders.csv")
 
+print(order_df.head(2))
+print("-"*40, "\n")
+print(order_df.tail(2))
+print("-"*40, "\n")
+order_df.info()
+print("-"*40, "\n")
+print(order_df.columns.tolist())
+print("-"*40, "\n")
+
+# 更改欄位名稱
+order_df = order_df.rename(columns={"飲料": "品名"})
+
+print(order_df["品名"])
+print("-"*40, "\n")
 print(order_df)
