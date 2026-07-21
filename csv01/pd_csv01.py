6bf67a49155bfaeac0f97f2af95837a2a2a79f3d
Ben Chen <idben.chen@gmail.com>
2026-07-21 15:24:56 +0800

    更改多欄欄位名稱

--- csv01/pd_csv01.py
+++ csv01/pd_csv01.py
@@ -19,3 +19,16 @@ order_df = order_df.rename(columns={"飲料": "品名"})
 print(order_df["品名"])
 print("-"*40, "\n")
 print(order_df)
+print("-"*40, "\n")
+
+# 更改欄位名稱: 改英文欄位名稱
+orders2_df = pd.read_csv("orders_en.csv")
+
+renamed_df = orders2_df.rename(columns={
+    "order_id": "訂單編號",
+    "item": "品名",
+    "qty": "數量",
+    "price": "單價"
+})
+
+print(renamed_df)
