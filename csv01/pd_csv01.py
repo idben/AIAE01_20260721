735dd50b9e29f23e8a54f62366417f577498da7c
Ben Chen <idben.chen@gmail.com>
2026-07-21 15:52:04 +0800

    複數條件

--- csv01/pd_csv01.py
+++ csv01/pd_csv01.py
@@ -39,3 +39,34 @@ print("-"*40, "\n")
 order_df["小計"] = order_df["數量"] * order_df["單價"]
 print(order_df, "\n")
 
+
+# 篩選: 下條件
+# 布林 mask
+
+condition = order_df["數量"] >= 2
+result_df = order_df[condition]
+print("數量大於兩杯的有: ")
+print(result_df)
+
+# 練習 1: \
+# condition1 取出單價大於等於 40 元的資料
+# condition2 取出小計大於等於 100 元的資料
+print("-"*40, "\n")
+
+
+# 複數條件
+# 而且 &
+is_milk_tea = order_df["品名"] == "奶茶"
+is_qty_enough = order_df["數量"] >= 2
+filtered_df = order_df[is_milk_tea & is_qty_enough]
+print("是奶茶而且兩杯(含)以上的訂單有: ")
+print(filtered_df)
+print("-"*40, "\n")
+
+# 或者 |
+is_black_tea = order_df["品名"] == "紅茶"
+is_price_45 = order_df["單價"] >= 45
+filtered2_df = order_df[is_black_tea | is_price_45]
+print("是紅茶, 或是單價大於等於 45 元的訂單有")
+print(filtered2_df)
+print("-"*40, "\n")
